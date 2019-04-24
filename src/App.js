28ca848e2b49ddc62f9dc26b73fd6129d602a7f3
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-24 13:45:57 +0200

    Add dummy code trying to replicate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import EventListener from "react-event-listener";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <EventListener target="document" />
+      <Grid container={true} alignItems="stretch" justify="center">
+        <Grid item={true}>SomeText</Grid>
+        <Grid item={true}>
+          <img alt="some image" href="etc" />
+        </Grid>
+      </Grid>
+      <Grid container={true} alignItems="stretch" justify="center">
+        <Grid item={true} xs={12}>
+          Some item
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
